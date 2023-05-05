36ba1e25818b12a6c772edfe57f6695e676283e9
Luís H. Gonçalves de Oliveira <109926443+Luisinhoo@users.noreply.github.com>
2023-05-05 12:46:05 -0300

    Atividades Iniciais

--- LAB01/Atividade 10.c
+++ LAB01/Atividade 10.c
@@ -1,9 +1,9 @@
-/*
+/**
    Atividade 10: Leia uma velocidade em km/h (quilômetros por hora) e apresente-a convertida em m/s
                  (metros por segundo). A fórmula de conversão é: M = K/3.6, sendo K a velocidade em km/h e M
                  em m/s.
    Por Luís H.
-*/
+**/
 #include <stdio.h> 
  
 int main() 
